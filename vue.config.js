609a1c98f79e6f7ef4fd3ec344e079a6740e067e
Russell Gapasin <russell@fanswifi.com>
2021-10-17 12:57:24 +0800

    Added pages metadata to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/recipe-app/'
-  : '/'
+  : '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      title: 'HomeChef | Become Your Own Chef',
+    },
+  }
 }
 
