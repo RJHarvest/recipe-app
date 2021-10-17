26564864fd345dbf4337ca4010355c429791a59c
Russell Gapasin <russell@fanswifi.com>
2021-10-17 12:08:19 +0800

    Modified main app component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App'
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    /* color: #2c3e50; */
+    color: #2C2928;
+    height: 100vh;
+    /* background-color: #DB9639; */
+    background: linear-gradient(to bottom right, rgba(219, 150, 57, 1), rgba(219, 150, 57, 0.8) 70%);
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+  .md-button.md-raised {
+    background-color: #2C2928 !important;
+    color: #ffffff !important;
+  }
 </style>
