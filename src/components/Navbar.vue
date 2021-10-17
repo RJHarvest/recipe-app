454763f0d8dd570e7631a7f0c1d1e7ea9ab97a62
Russell Gapasin <russell@fanswifi.com>
2021-10-17 12:05:35 +0800

    Added navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <md-toolbar md-elevation="0">
+    <div class="md-toolbar-section-start">
+      <img class="logo" src="@/assets/logo-small-transparent.png" alt="logo" />
+      <md-button to="/">Home</md-button>
+      <md-button to="/cuisine">Cuisine</md-button>
+      <md-button to="/recipe">Recipes</md-button>
+      <md-button to="/meal">Meals</md-button>
+    </div>
+
+    <div class="md-toolbar-section-end">
+      <md-button class="md-icon-button">
+        <md-icon>search</md-icon>
+      </md-button>
+    </div>
+  </md-toolbar>
+</template>
+
+<script>
+  export default {
+    name: 'navbar',
+  }
+</script>
+
+<style scoped>
+  .md-toolbar {
+    background: none !important;
+    padding: 20px 25px;
+  }
+  .logo {
+    height: 70px;
+  }
+</style>
