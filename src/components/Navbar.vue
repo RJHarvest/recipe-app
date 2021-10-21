a3a760722b201c901b43e14d3c700b24754d6d9a
Russell Gapasin <russell@fanswifi.com>
2021-10-22 00:58:47 +0800

    Added search function to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,44 @@
 <template>
-  <md-toolbar md-elevation="0">
-    <div class="md-toolbar-section-start">
-      <img class="logo" src="@/assets/logo-small-transparent.png" alt="logo" />
-      <md-button to="/">Home</md-button>
-      <md-button to="/recipe">Recipes</md-button>
-    </div>
+  <div>
+    <md-toolbar md-elevation="0">
+      <div class="md-toolbar-section-start">
+        <img class="logo" src="@/assets/logo-small-transparent.png" alt="logo" />
+        <md-button to="/">Home</md-button>
+        <md-button to="/recipe">Recipes</md-button>
+      </div>
 
-    <div class="md-toolbar-section-end">
-      <md-button class="md-icon-button">
-        <md-icon>search</md-icon>
-      </md-button>
-    </div>
-  </md-toolbar>
+      <div class="md-toolbar-section-end">
+        <md-button class="md-icon-button" @click="showSearchBar = true">
+          <md-icon>search</md-icon>
+        </md-button>
+      </div>
+    </md-toolbar>
+    <!-- Search popup -->
+    <md-dialog-prompt
+      :md-active.sync="showSearchBar"
+      v-model="search"
+      md-title="Search for recipes"
+      md-input-placeholder="Enter recipe name..."
+      md-confirm-text="Search"
+      @md-confirm="handleSearchOnConfirm"
+    />
+  </div>
 </template>
 
 <script>
   export default {
     name: 'navbar',
+    data() {
+      return {
+        showSearchBar: false,
+        search: null,
+      }
+    },
+    methods: {
+      handleSearchOnConfirm(search) {
+        this.$router.push({ name: 'recipe', query: { search } })
+      }
+    }
   }
 </script>
 
