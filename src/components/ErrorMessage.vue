00ba20e71bb26f4e9c98c2537eb0fa4105471565
Russell Gapasin <russell@fanswifi.com>
2021-10-22 01:00:57 +0800

    Added error message component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <md-snackbar md-position="center" :md-duration="snackbarDuration" :md-active.sync="showError">
+    <span>{{ error }}</span>
+  </md-snackbar>
+</template>
+
+<script>
+  export default {
+    name: 'error-message',
+    props: {
+      showError: Boolean,
+      error: String,
+    },
+    data() {
+      return {
+        snackbarDuration: 4000,
+      }
+    },
+  }
+</script>
